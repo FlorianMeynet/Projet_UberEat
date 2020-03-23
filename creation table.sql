cfb3f850a837590fedfd2d24205bca2ab6d52c0b
Florian Menyet <meynet.florian@orange.fr>
2020-03-23 16:18:21 +0100

    mysql

---
+++ creation table.sql
@@ -0,0 +1,69 @@
+create database cooking;
+USE `cooking`;
+CREATE TABLE `cooking`.`client` (
+  `nom` VARCHAR(20) NOT NULL,
+ " `prenom` VARCHAR(20) NULL,
+  `age` INT NULL,
+  `permis` VARCHAR(10) NULL,
+  `adresse` VARCHAR(50) NULL,
+  `ville` VARCHAR(20) NULL,"
+  PRIMARY KEY (`codeC`) );
+  
+CREATE TABLE `cooking`.`fournisseur` (
+  `nomF` VARCHAR(20) NOT NULL,
+  `tel` VARCHAR(20) NULL,
+  `age` INT NULL,
+  `permis` VARCHAR(10) NULL,
+  `adresse` VARCHAR(50) NULL,
+  `ville` VARCHAR(20) NULL,
+  PRIMARY KEY (`codeC`) );
+CREATE TABLE `cooking`.`createur` (
+  `codeP` VARCHAR(4) NOT NULL,
+  `pseudo` VARCHAR(20) NOT NULL,
+  `email` VARCHAR(20) NULL,
+  `ville` VARCHAR(20) NULL,
+  `anneeI` VARCHAR(4) NULL,
+  PRIMARY KEY (`codeP`) );
+  
+CREATE TABLE `cooking`.`recette` (
+  `immat` VARCHAR(10) NOT NULL,
+  `modele` VARCHAR(20) NULL,
+  `marque` VARCHAR(20) NULL,
+  `categorie` VARCHAR(20) NULL,
+  `couleur` ENUM('blanc','rouge','vert','noir','violet','bleu'),
+  `places` INT NULL CHECK (places <= 7),
+  `achatA` VARCHAR(4) NULL,
+  `compteur` INT NOT NULL,
+  `prixJ` INT NULL,
+  `codeP` VARCHAR(4) NULL,
+  PRIMARY KEY (`immat`),
+   INDEX `F_voit1_idx` (`codeP` ASC),
+   CONSTRAINT `codePVoiture` FOREIGN KEY (`codeP`)
+		REFERENCES `loueur`.`proprietaire` (`codeP`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION );
+ 
+CREATE TABLE `cooking`.`produit` (
+  `codeC` VARCHAR(4) NOT NULL,
+  `immat` VARCHAR(10) NOT NULL,
+  `annee` VARCHAR(4) NOT NULL,
+  `mois` VARCHAR(2) NOT NULL,
+  `numLoc` VARCHAR(5) NULL,
+  `km` INT NOT NULL,
+  `duree` INT NULL,
+  `villeD` VARCHAR(20) NULL,
+  `villeA` VARCHAR(20) NULL,
+  `dateD` DATE NULL,
+  `dateF` DATE NULL,
+  PRIMARY KEY (`codeC`, `immat`, `annee`, `mois`),
+   INDEX `F_loc1_idx` (`codeC` ASC),
+   INDEX `F_loc2_idx` (`immat` ASC),
+   INDEX `F_loc3_idx` (`numLoc` ASC),
+   CONSTRAINT `codeCLocation` FOREIGN KEY (`codeC`)
+		REFERENCES `loueur`.`client` (`codeC`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+  CONSTRAINT `immatLocation` FOREIGN KEY (`immat`)
+		REFERENCES `loueur`.`voiture` (`immat`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION  );
