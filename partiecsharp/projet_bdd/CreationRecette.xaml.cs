0de0aa0468093790ac90729428a797a0e91c9e02
Slimtrat <loicmorinrichard@gmail.com>
2020-04-02 14:09:12 +0200

    Quelquesapports

--- partiecsharp/projet_bdd/CreationRecette.xaml.cs
+++ partiecsharp/projet_bdd/CreationRecette.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace projet_bdd
 {
@@ -24,7 +25,54 @@ namespace projet_bdd
 
         private void Validation(object sender, RoutedEventArgs e)
         {
-        //Création d'une nouvelle recette
+
+            MySqlConnection maConnexion = null;
+            try
+            {
+                string connexionString = "SERVER=localhost;PORT=3306;DATABASE=tableprojet;UID=root;PASSWORD=4F10e6bff@;";
+                maConnexion = new MySqlConnection(connexionString);
+                maConnexion.Open();
+            }
+            catch (MySqlException er)
+            {
+                Console.WriteLine(" ErreurConnexion : " + er.ToString());
+                return;
+            }
+
+
+            string nomrecettee = nomrecette.Text;
+            string descriptife = descriptif.Text;
+            float prixs = float.Parse(prix.Text);
+            string listeingrediente = listingredient.Text;
+
+            string requete = "Select Nom from Recette;";
+
+
+
+            MySqlCommand command1 = maConnexion.CreateCommand();
+            command1.CommandText = requete;
+
+            MySqlDataReader reader = command1.ExecuteReader();
+            command1.Dispose();
+            bool existe = false;
+            while (reader.Read())
+            {
+
+                if (nomrecettee == reader.GetValue(1).ToString()) //La valeur 0 c'est l'id, donc on prend la valeur 1 : le nom
+                {
+                    existe = true;
+                }
+
+            }
+            if (existe == false)
+            {
+                string requete2 = "insert into tableprojet.recette(`Nom`,`descriptif`,`prix`,`listingredient`) Values(" + nomrecette + ", " + descriptife + ", " + prixs + ", " + listeingrediente;
+                MySqlCommand command2 = maConnexion.CreateCommand();
+                command1.CommandText = requete2;
+
+                MySqlDataReader reader2 = command2.ExecuteReader();
+                command2.Dispose();
+            }
         }
 
         private void Retour(object sender, RoutedEventArgs e)
