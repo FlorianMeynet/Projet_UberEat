2af507f591ee449147aa01f95b8cf8b66af2fce8
Slimtrat <loicmorinrichard@gmail.com>
2020-04-03 11:19:34 +0200

    Petite modification apres reflexion

--- partiecsharp/projet_bdd/ClientStatic.cs
+++ partiecsharp/projet_bdd/ClientStatic.cs
@@ -4,36 +4,21 @@ using System.Text;
 
 namespace projet_bdd
 {
-    static class ClientStatic //L'avantage de cette classe c'est qu'elle est accessible interpage, du coup c'est pas mal, mais il faut appeler ClientStatic.ClientStatic1() ou ClientStatic.ClientStatic2(parametre) pour modifier ton ClientStatic, mais tu peux directement avoir tes attributs avec ClientStatic.attribut
+    static class ClientStatic //L'avantage de cette classe c'est qu'elle est accessible interpage, du coup c'est pas mal, mais il faut appeler ClientStatic.ClientStatic1(avec ou sans parametre)  pour modifier ton ClientStatic, mais tu peux directement avoir tes attributs avec ClientStatic.attribut
     {
             public static int idClient;
             public static string nom;
             public static string prenom;
-        public static string adresse;
-        public static string ville;
-        public static DateTime? d_naissance;
-        public static long tel;
-        public static string mail;
-        public static bool estCreateur;
-        public static int capitalCooks;
-        public static string mdp;
-            static public void ClientStatic1()
-            {
-            
-                ClientStatic.idClient = 0;
-                ClientStatic.nom = null;
-                ClientStatic.prenom = null;
-            ClientStatic.adresse = null;
-            ClientStatic.ville = null;
-            ClientStatic.d_naissance = null;
-            ClientStatic.tel = 0;
-            ClientStatic.mail = null;
-            ClientStatic.estCreateur = false;
-            ClientStatic.capitalCooks = 0;
-            ClientStatic.mdp = null;
-            }
+            public static string adresse;
+            public static string ville;
+            public static DateTime? d_naissance;
+            public static long tel;
+            public static string mail;
+            public static bool estCreateur;
+            public static int capitalCooks;
+            public static string mdp;
 
-            static public void ClientStatic2(int i, string n, string p, string a, string v, DateTime? d, long t, string m, bool e, int c, string mdp)
+            static public void ClientStatic1(int i=0, string n=null, string p=null, string a=null, string v=null, DateTime? d=null, long t=0, string m=null, bool e=false, int c=0, string mdp=null)
             {
             ClientStatic.idClient = i;
             ClientStatic.nom = n;
