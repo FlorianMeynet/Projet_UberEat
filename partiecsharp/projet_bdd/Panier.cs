a7718a27376277750242dae8c80fda57fb73fda1
Slimtrat <loicmorinrichard@gmail.com>
2020-04-10 10:10:12 +0200

    Ajout d'une classe

---
+++ partiecsharp/projet_bdd/Panier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projet_bdd
+{
+    public static class Panier
+    {
+        public static int idClient;
+        public static float Valeur;
+        public static List<int> listIdIngredient =new List<int>();
+    }
+}
